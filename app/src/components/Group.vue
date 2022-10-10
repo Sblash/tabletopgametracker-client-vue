b9e7e1744c57a2583737892f1c9bfdcb8d43c778
Stefano <stecre@live.it>
2022-10-10 23:44:15 +0200

    added group component (to finish)

---
+++ app/src/components/Group.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+        
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Group',
+  components: {
+
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
