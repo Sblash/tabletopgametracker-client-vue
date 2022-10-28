92bc7fe2d3752b5c0fb74362c376ee7a0c8677f9
Sblash <stecre@live.it>
2022-10-28 19:39:40 +0200

    implement Structurer base logic

--- app/src/components/Structurer.vue
+++ app/src/components/Structurer.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="row structurer">
+  <div class="row">
     <div class="col-md-12">
-
+      <div class="row add_row" @click="addRow">
+        <div class="col-md-12 add_button">
+          <span>Aggiungi riga</span>
+        </div>
+      </div>
+      <div class="row row_strucure" v-for="(row, rowIndex) in structure.rows" :key="row">
+        <div class="col-md-12 add_button margin_bottom" @click="addColumn(rowIndex)" v-if="this.structure.rows[rowIndex].cols.length < 3">
+          <span>Aggiungi colonna</span>
+        </div>
+        <div class="col_structure" :class="getClass(rowIndex)" v-for="(col, colIndex) in structure.rows[rowIndex].cols" :key="col">
+          <div class="add_button margin_bottom" @click="addElement(rowIndex, colIndex)">
+            <span>Aggiungi elemento</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,38 +26,75 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Structurer',
-  props: {
-
-  },
-  computed: {
-    
+  data() {
+    return {
+      structure: {
+        rows: []
+      }
+    }
   },
   methods: {
-    
+    addRow() {
+      const row = {
+        cols: []
+      };
+      this.structure.rows.push(row);
+    },
+    addColumn(index: number) {
+      const col = {
+        elements: []
+      };
+
+      this.structure.rows[index].cols.push(col);
+    },
+    addElement(rowIndex: number, colIndex: number) {
+      const element = {};
+      this.structure.rows[rowIndex].cols[colIndex].elements.push(element);
+    },
+    getClass(rowIndex: number) {
+      const arrayColsSize = this.structure.rows[rowIndex].cols.length;
+      let cols = 12 / arrayColsSize
+      return "col-md-" + cols;
+    }
   }
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.structurer {
-  border-radius: 20px;
+.add_row {
+  margin-left: 0px;
+  margin-right: 0px;
+  margin-bottom: 16px;
+}
+
+.add_button {
+  border-radius: 10px;
   border: 1px dashed grey;
   background-color: lightgray;
   padding: 8px;
   max-height: 90px;
   cursor: pointer;
+  text-align: center;
+}
+
+.margin_bottom {
   margin-bottom: 16px;
 }
 
-.structurer_img {
-  border-radius: 50px;
-  width: 70px;
-  height: 70px;
+.row_strucure {
+  border: 1px solid black;
+  border-radius: 10px;
+  margin-left: 0px;
+  margin-right: 0px;
+  padding: 8px;
+  margin-bottom: 16px;
 }
 
-.structurer_name {
-  font-size: 30px;
-  line-height: 2.2;
+.col_structure {
+  border: 1px solid black;
+  border-radius: 10px;
+  margin-bottom: 16px;
+  padding: 8px;
 }
 </style>
